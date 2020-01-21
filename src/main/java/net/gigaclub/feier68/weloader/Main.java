8751c34c3a09a1873ca20069e87a09f101169cf7
Ricardo <rici.rr8@gmail.com>
2020-01-21 18:54:14 +0100

    add config add einstellbare world schematic loade add einstellbare nehter schematic loade

--- src/main/java/net/gigaclub/feier68/weloader/Main.java
+++ src/main/java/net/gigaclub/feier68/weloader/Main.java
@@ -28,23 +28,26 @@ public final class Main extends JavaPlugin {
     private static Main plugin;
 
     public static boolean loadIslandSchematic() throws IOException {
+        String pl = "[WEloader] ";
         FileConfiguration config = Main.getPlugin().getConfig();
         if (!(config.isSet("Load.world"))) {
             System.out.println("Load World Config Wurde gesetzt");
             config.set("Load.world", true);
             Main.getPlugin().saveConfig();
         }
+        String worldSchem = config.getString("Schematic.World");
         boolean worldSpawn = config.getBoolean("Load.world");
-        File file = new File("plugins/WorldEdit/schematics/island.schem");
-
-        if (file.exists()) {
-            System.out.printf("World");
-            System.out.println("File exists");
-        } else {
-            System.out.println("Nothing....");
-        }
-        System.out.println("world " + worldSpawn);
         if (worldSpawn) {
+            File file = new File("plugins/WEloader/schematics/" + worldSchem);
+            if (file.exists()) {
+                System.out.printf(pl + "World");
+                System.out.println(pl + "File exists");
+            } else {
+                System.out.println(pl + "File " + worldSchem + " dont exists");
+                return true;
+            }
+
+
             World world = Bukkit.getWorld("world");
             ClipboardFormat format = ClipboardFormats.findByFile(file);
             try (ClipboardReader reader = format.getReader(new FileInputStream(file))) {
@@ -57,63 +60,72 @@ public final class Main extends JavaPlugin {
                             .ignoreAirBlocks(false)
                             .build();
                     Operations.complete(operation);
-                    System.out.printf("Schematic Geladen");
+                    System.out.printf(pl + "Schematic Geladen");
                 } catch (WorldEditException e) {
                     e.printStackTrace();
                 }
             }
 
         }
-        return true;
 
+        return true;
     }
     public static boolean nehterSchematic() throws IOException {
+        String pl = "[WEloader] ";
         FileConfiguration config = Main.getPlugin().getConfig();
         if (!(config.isSet("Load.nether"))) {
-            System.out.println("Load Nether Config Wurde gesetzt");
+            System.out.println(pl + "Load Nether Config Wurde gesetzt");
             config.set("Load.nether", false);
             Main.getPlugin().saveConfig();
         }
+        String nehterSchem = config.getString("Schematic.Nether");
+        boolean worldSpawn = config.getBoolean("Load.world");
         boolean nehterSpawn = config.getBoolean("Load.nether");
-        File file = new File("plugins/WorldEdit/schematics/NeterSpawn.schem");
-
-        if (file.exists()) {
-            System.out.printf("Nehter");
-            System.out.println("File exists");
-        } else {
-            System.out.println("Nothing....");
-        }
 
         if (nehterSpawn) {
-            System.out.println("Nehtercreate" + nehterSpawn);
-            World world = Bukkit.getWorld("world_nether");
-            ClipboardFormat format = ClipboardFormats.findByFile(file);
-            try (ClipboardReader reader = format.getReader(new FileInputStream(file))) {
-                Clipboard clipboard = reader.read();
+            File file = new File("plugins/WEloader/schematics/" + nehterSchem);
 
-                try (EditSession editSession = WorldEdit.getInstance().getEditSessionFactory().getEditSession((BukkitAdapter.adapt(world)), -1)) {
-                    Operation operation = new ClipboardHolder(clipboard)
-                            .createPaste(editSession)
-                            .to(BlockVector3.at(0, 100, 0))
-                            .ignoreAirBlocks(false)
-                            .build();
-                    Operations.complete(operation);
-                    System.out.printf("Schematic Geladen");
-                } catch (WorldEditException e) {
-                    e.printStackTrace();
+            if (file.exists()) {
+                System.out.printf(pl + "Nehter");
+                System.out.println(pl + "File exists");
+            } else {
+                System.out.println(pl + "File " + nehterSchem + " dont exists");
+                return true;
+            }
+
+
+            if (!worldSpawn) {
+                World world = Bukkit.getWorld("world_nether");
+                ClipboardFormat format = ClipboardFormats.findByFile(file);
+                try (ClipboardReader reader = format.getReader(new FileInputStream(file))) {
+                    Clipboard clipboard = reader.read();
+
+                    try (EditSession editSession = WorldEdit.getInstance().getEditSessionFactory().getEditSession((BukkitAdapter.adapt(world)), -1)) {
+                        Operation operation = new ClipboardHolder(clipboard)
+                                .createPaste(editSession)
+                                .to(BlockVector3.at(0, 100, 0))
+                                .ignoreAirBlocks(false)
+                                .build();
+                        Operations.complete(operation);
+                        System.out.printf(pl + "Schematic Geladen");
+                    } catch (WorldEditException e) {
+                        e.printStackTrace();
+                    }
                 }
+
             }
 
         }
+
+
         return true;
-
-
     }
 
 
     @Override
     public void onDisable() {
-        System.out.printf("BB");
+        String pl = "[WEloader] ";
+        System.out.printf(pl + "BB");
     }
 
     public static Main getPlugin() {
@@ -123,17 +135,31 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         plugin = this;
+        String pl = "[WEloader] ";
+
+
         new File("plugins/WEloader/schematics").mkdir();
         BlockVector3 min = BlockVector3.at(-30, 150, -30);
         BlockVector3 max = BlockVector3.at(30, -8, 30);
         ProtectedCuboidRegion region = new ProtectedCuboidRegion("spawn", min, max);
 
-        System.out.printf("WELoader Geladen");
+        System.out.printf(pl + "WELoader Geladen");
+
         FileConfiguration config = Main.getPlugin().getConfig();
-        if (config.isSet("Load.world"))
-            config.set("Load.world", true);
-        if (config.isSet("Load.nether"))
-            config.set("Load.nether", false);
+        if (!(config.isSet("Schematic.Nether"))) {
+            config.set("Schematic.Nether", "NeterSpawn.schem");
+        }
+        Main.getPlugin().saveConfig();
+        if (!(config.isSet("Schematic.World"))) {
+            config.set("Schematic.World", "island.schem");
+        }
+        Main.getPlugin().saveConfig();
+
+        boolean nehterSpawn = config.getBoolean("Load.nether");
+        boolean worldSpawn = config.getBoolean("Load.world");
+        if (nehterSpawn)
+            if (worldSpawn)
+                System.out.println(pl + "es sind beide activ");
 
         PluginManager pluginManager = Bukkit.getPluginManager();
         pluginManager.registerEvents(new JoinListener(), this);
@@ -147,7 +173,6 @@ public final class Main extends JavaPlugin {
         }
 
     }
-
 }
 
 
--- src/main/java/net/gigaclub/feier68/weloader/listeners/JoinListener.java
+++ src/main/java/net/gigaclub/feier68/weloader/listeners/JoinListener.java
@@ -1,8 +1,10 @@
 package net.gigaclub.feier68.weloader.listeners;
 
+import net.gigaclub.feier68.weloader.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,24 +14,35 @@ public class JoinListener implements Listener {
 
    @EventHandler
    public void handlePlayerJoin(PlayerJoinEvent event) {
-      Player player = event.getPlayer();
-      World world = Bukkit.getWorld("world");
-      Location location = new Location(world, 0.0, 101, 0.0);
+      FileConfiguration config = Main.getPlugin().getConfig();
+      boolean worldSpawn = config.getBoolean("Load.world");
+      if (worldSpawn) {
+         Player player = event.getPlayer();
+         World world = Bukkit.getWorld("world");
+         Location location = new Location(world, 0.0, 126, 0.0);
 
-      player.teleport(location);
+         player.teleport(location);
 
-
+      }
    }
 
    @EventHandler
    public void NehterhandlePlayerJoin(PlayerJoinEvent event) {
-      Player player = event.getPlayer();
-      World world = Bukkit.getWorld("world_nether");
-      Location location = new Location(world, 0.0, 101, 0.0);
+      FileConfiguration config = Main.getPlugin().getConfig();
+      boolean nehterSpawn = config.getBoolean("Load.nether");
+      boolean worldSpawn = config.getBoolean("Load.world");
 
-      player.teleport(location);
+      if (worldSpawn) {
+         return;
+      } else if (nehterSpawn) {
+         Player player = event.getPlayer();
+         World world = Bukkit.getWorld("world_nether");
+         Location location = new Location(world, 0.5, 101, 0.5);
 
+         player.teleport(location);
+
+      }
+   }
 
    }
 
-}

314901a2ab505af7ee94f44137015cf59ce91a96
Ricardo <rici.rr8@gmail.com>
2020-03-16 12:32:46 +0100

    erster falldmg entfernen

--- src/main/java/net/gigaclub/feier68/weloader/Main.java
+++ src/main/java/net/gigaclub/feier68/weloader/Main.java
@@ -12,13 +12,13 @@ import com.sk89q.worldedit.function.operation.Operation;
 import com.sk89q.worldedit.function.operation.Operations;
 import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.session.ClipboardHolder;
-import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
 import net.gigaclub.feier68.weloader.listeners.JoinListener;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -71,6 +71,9 @@ public final class Main extends JavaPlugin {
 
         return true;
     }
+
+    int count = 0;
+
     public static boolean nehterSchematic() throws IOException {
         String pl = "[WEloader] ";
         FileConfiguration config = Main.getPlugin().getConfig();
@@ -119,10 +122,54 @@ public final class Main extends JavaPlugin {
 
         return true;
     }
-    @Override
-    public void onDisable() {
+
+    public static boolean endSchematic() throws IOException {
         String pl = "[WEloader] ";
-        System.out.println(pl + "BB");
+        FileConfiguration config = Main.getPlugin().getConfig();
+        if (!(config.isSet("Load.end"))) {
+            System.out.println(pl + "Load End Config Wurde gesetzt");
+            config.set("Load.end", false);
+            Main.getPlugin().saveConfig();
+        }
+        String endSchem = config.getString("Schematic.End");
+        boolean worldSpawn = config.getBoolean("Load.world");
+        boolean endSpawn = config.getBoolean("Load.end");
+
+        if (endSpawn) {
+            File file = new File("plugins/WEloader/schematics/" + endSchem);
+
+            if (file.exists()) {
+                System.out.println(pl + "End");
+                System.out.println(pl + "File exists");
+            } else {
+                System.out.println(pl + "File " + endSchem + " dont exists");
+                return true;
+            }
+            if (!worldSpawn) {
+                World world = Bukkit.getWorld("world_the_end");
+                ClipboardFormat format = ClipboardFormats.findByFile(file);
+                try (ClipboardReader reader = format.getReader(new FileInputStream(file))) {
+                    Clipboard clipboard = reader.read();
+
+                    try (EditSession editSession = WorldEdit.getInstance().getEditSessionFactory().getEditSession((BukkitAdapter.adapt(world)), -1)) {
+                        Operation operation = new ClipboardHolder(clipboard)
+                                .createPaste(editSession)
+                                .to(BlockVector3.at(0, 70, 80))
+                                .ignoreAirBlocks(false)
+                                .build();
+                        Operations.complete(operation);
+                        System.out.println(pl + "Schematic Geladen");
+                    } catch (WorldEditException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+
+        }
+
+
+        return true;
     }
 
     public static Main getPlugin() {
@@ -130,28 +177,35 @@ public final class Main extends JavaPlugin {
     }
 
     @Override
+    public void onDisable() {
+        String pl = "[WEloader] ";
+        System.out.println(pl + "auf wider sehen");
+    }
+
+    @Override
     public void onEnable() {
         plugin = this;
         String pl = "[WEloader] ";
-        new File("plugins/WEloader/schematics").mkdir();
-        BlockVector3 min = BlockVector3.at(-30, 150, -30);
-        BlockVector3 max = BlockVector3.at(30, -8, 30);
-        ProtectedCuboidRegion region = new ProtectedCuboidRegion("spawn", min, max);
 
         System.out.println(pl + "WELoader Geladen");
 
         FileConfiguration config = Main.getPlugin().getConfig();
         if (!(config.isSet("Schematic.Nether"))) {
-            config.set("Schematic.Nether", "NeterSpawn.schem");
+            config.set("Schematic.Nether", "NetherSpawn.schem");
         }
         Main.getPlugin().saveConfig();
         if (!(config.isSet("Schematic.World"))) {
-            config.set("Schematic.World", "island.schem");
+            config.set("Schematic.World", "WorldSpawn.schem");
+        }
+        Main.getPlugin().saveConfig();
+        if (!(config.isSet("Schematic.end"))) {
+            config.set("Schematic.End", "EndSpawn.schem");
         }
         Main.getPlugin().saveConfig();
 
         boolean nehterSpawn = config.getBoolean("Load.nether");
         boolean worldSpawn = config.getBoolean("Load.world");
+
         if (nehterSpawn)
             if (worldSpawn)
                 System.out.println(pl + "es sind beide activ");
@@ -160,9 +214,30 @@ public final class Main extends JavaPlugin {
         try {
             loadIslandSchematic();
             nehterSchematic();
+            endSchematic();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
+        BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
+        scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
+            @Override
+            public void run() {
+                int mincount = count / 60;
+                if (count % 360 * 60 == 0 || count == 1260 * 60 || count == 1320 * 60 || count == 1380 * 60) {
+                    Bukkit.broadcastMessage("Die Farmwelt resettet in " + (24 - (mincount / 60)) + " Stunden!");
+                } else if (count == 1395 * 60 || count == 1410 * 60 || count == 1425 * 60 || count == 1430 * 60 || count == 1435 * 60 || count == 1439 * 60) {
+                    Bukkit.broadcastMessage("Die Farmwelt resettet in " + (1440 - mincount) + " Minuten!");
+                } else if (count > 86394) {
+                    Bukkit.broadcastMessage("Die Farmwelt resettet in " + (86400 - count) + " Sekunden!");
+                } else if (mincount == 1440) {
+                    Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "stop");
+                }
+                count++;
+
+            }
+        }, 0, 20);
 
     }
 
--- src/main/java/net/gigaclub/feier68/weloader/listeners/JoinListener.java
+++ src/main/java/net/gigaclub/feier68/weloader/listeners/JoinListener.java
@@ -3,14 +3,24 @@ package net.gigaclub.feier68.weloader.listeners;
 import net.gigaclub.feier68.weloader.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+import java.util.HashMap;
+
 
 public class JoinListener implements Listener {
+
+   HashMap<Player, Integer> fallcount = new HashMap<>();
 
    @EventHandler
    public void handlePlayerJoin(PlayerJoinEvent event) {
@@ -18,12 +28,18 @@ public class JoinListener implements Listener {
       boolean worldSpawn = config.getBoolean("Load.world");
       if (worldSpawn) {
          Player player = event.getPlayer();
+         if (fallcount.get(player) != null) {
+            fallcount.remove(player);
+         }
+         fallcount.put(player, 0);
+
          World world = Bukkit.getWorld("world");
          Location location = new Location(world, 0.0, 126, 0.0);
-
          player.teleport(location);
 
       }
+
+
    }
 
    @EventHandler
@@ -36,13 +52,78 @@ public class JoinListener implements Listener {
          return;
       } else if (nehterSpawn) {
          Player player = event.getPlayer();
+         if (fallcount.get(player) != null) {
+            fallcount.remove(player);
+         }
+         fallcount.put(player, 0);
          World world = Bukkit.getWorld("world_nether");
          Location location = new Location(world, 0.5, 101, 0.5);
-
          player.teleport(location);
 
       }
    }
 
+   @EventHandler
+   public void endPlayerJoin(PlayerJoinEvent event) {
+      FileConfiguration config = Main.getPlugin().getConfig();
+      boolean worldSpawn = config.getBoolean("Load.end");
+      if (worldSpawn) {
+         Player player = event.getPlayer();
+         if (fallcount.get(player) != null) {
+            fallcount.remove(player);
+         } else {
+            fallcount.put(player, 0);
+
+         }
+
+         World world = Bukkit.getWorld("world_the_end");
+         Location location = new Location(world, 0.0, 71, 80.0);
+         player.teleport(location);
+
+      }
    }
+
+
+   @EventHandler
+   public void dmg(final EntityDamageEvent event) {
+      FileConfiguration config = Main.getPlugin().getConfig();
+
+
+      if (event.getCause() != EntityDamageEvent.DamageCause.FALL) {
+      } else {
+
+         Entity e = event.getEntity();
+         if (e instanceof Player) {
+            Player player = (Player) e;
+
+
+            fallcount.replace(player, fallcount.get(player) + 1);
+            if (fallcount.get(player) == 1) {
+               event.setCancelled(true);
+               player.sendMessage("Healing spell");
+            }
+         }
+      }
+   }
+
+
+   @EventHandler
+   public void PlayerMove(PlayerMoveEvent event) {
+      Player player = event.getPlayer();
+      Location from = event.getFrom();
+      Location to = event.getTo();
+      if (!to.getWorld().getBlockAt(to).equals(from.getWorld().getBlockAt(from))) // checks if you moved at least one block
+      {
+         Block checkWater = event.getPlayer().getLocation().getBlock();
+         if (checkWater.getType() == Material.WATER) {
+            fallcount.replace(player, fallcount.get(player) + 1);
+         }
+
+      }
+   }
+
+}
+
+
+
 
